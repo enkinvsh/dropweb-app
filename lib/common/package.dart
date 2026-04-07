bb0e627cf98db4c3c22e5ebccabccde31de60180
Alexey <enkinvsh@gmail.com>
2026-04-07 22:53:56 +0500

    fix: add clash-verge UA prefix for Remnawave subscription compat

--- lib/common/package.dart
+++ lib/common/package.dart
@@ -4,6 +4,7 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 extension PackageInfoExtension on PackageInfo {
   String get ua => [
+        "clash-verge/v$version",
         "dropweb/v$version",
         "Platform/${Platform.operatingSystem}",
       ].join(" ");
