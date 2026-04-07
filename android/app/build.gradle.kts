69c59e42f0b5fcb1b78b4564e40ff533e9a4f4e8
Alexey <enkinvsh@gmail.com>
2026-04-07 22:20:20 +0500

    fix: minSdk 21 → 23 to match core module

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
     defaultConfig {
         applicationId = "org.dropweb.vpn"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
