43ab11b02795728de67c3ac58143c8012bf85e2f
Alexey <enkinvsh@gmail.com>
2026-04-11 22:02:20 +0500

    fix(android): bump minSdk to 23 to match core module

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
     defaultConfig {
         applicationId = "app.dropweb"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
